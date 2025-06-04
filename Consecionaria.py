d43c59e91ee237fed79bfb4419e9531aae167092
EdgarO <ed.orozco0@gmail.com>
2025-06-04 12:37:00 -0600

    Proyecto concesionaria completo

--- Consecionaria.py
+++ Consecionaria.py
@@ -10,21 +10,56 @@ class Car:
         self.available_for_sell = True
 
     def rent(self):
-    if self.available_for_rent:
-        self.available_for_rent = False
-        print(f"El auto {self.brand}, {self.model} ha sido rentado")
-    else:
-        print(f"Lo sentimos, el auto {self.brand}, {self.model} no está disponible para rentar en este momento")
+        if self.available_for_rent:
+            self.available_for_rent = False
+            print(f"El auto {self.brand}, {self.model} ha sido rentado")
+        else:
+            print(f"Lo sentimos, el auto {self.brand}, {self.model} no está disponible para rentar en este momento")
 
     def sell(self):
-        pass
+        if self.available_for_sell:
+            self.available_for_sell = False
+            print(f"El auto {self.brand}, {self.model} ha sido vendido")
+        else:
+            print(f"Lo sentimos, el auto {self.brand}, {self.model} no está disponible para vender en este momento")
+
+    def reBuy(self):
+        self.available_for_sell = True
+        print(f"El auto {self.brand}, {self.model} ha sido devuelto y está disponible para la venta nuevamente")
 
 class User:
     def __init__(self, id, name, surname):
         self.id = id
         self.name = name
         self.surname = surname
+        self.rented_cars = []
+        self.purchased_cars = []
 
+    def rentCar(self, car):
+        if car.available_for_rent: # Primero verificas si el auto está disponible
+            car.rent() # Si está disponible, lo rentas (y esto lo marca como no disponible)
+            self.rented_cars.append(car) # Y lo añades a la lista del usuario
+            print(f"El usuario {self.name} ha rentado el auto {car.brand}, {car.model}")
+        else:
+            # Si no estaba disponible, solo imprimes el mensaje de no disponible
+            print(f"Lo sentimos, el auto {car.brand}, {car.model} no está disponible para rentar en este momento")
+            # No haces nada más, porque el auto no se pudo rentar.
+
+    def rentCar(self, car):
+        car.rent()
+        if car.available_for_rent:
+            self.rented_cars.append(car)
+            print(f"El usuario {self.name} ha rentado el auto {car.brand}, {car.model}")
+        else:
+            print(f"Lo sentimos, el auto {car.brand}, {car.model} no está disponible para rentar en este momento")
+
+    def buyCar(self, car):
+        car.sell()
+        if car.available_for_sell:
+            self.purchased_cars.append(car)
+            print(f"El usuario {self.name} ha comprado el auto {car.brand}, {car.model}")
+        else:
+            print(f"Lo sentimos, el auto {car.brand}, {car.model} no está disponible para vender en este momento")
 
 
 
@@ -32,8 +67,71 @@ class User:
 
 class Concesionaria:
     def __init__(self):
-        self.cars = cars[]
-        self.users = users[]
+        self.cars = []
+        self.users = []
+        self.rented_cars = []
+        self.sold_cars = []
+        self.rebuy_cars = []
+        
+    def addCar(self, car):
+        self.cars.append(car)
+        print(f"El auto {car.brand}, {car.model} ha sido agregado a la concesionaria")
+
+    def registerUser(self, user):
+        self.users.append(user)
+        print(f"El usuario {user.name} ha sido registrado en la concesionaria")
+
+    def showAvailableCars(self):
+        print("Autos disponibles:")
+        for car in self.cars:
+            if car.available_for_rent or car.available_for_sell:
+                print(f"{car.brand}, {car.model}, {car.year}, ${car.price}")
+
+
+#Register a car
+car1 = Car("Toyota", "Corolla", 2020, 20000)
+car2 = Car("Honda", "Civic", 2021, 22000)
+car3 = Car("Ford", "Focus", 2019, 18000)
+car4 = Car("Chevrolet", "Cruze", 2022, 25000)
+#Create a user
+user1 = User(1, "Juan", "Perez")
+user2 = User(2, "Maria", "Gomez")
+
+#Create a dealership
+dealership = Concesionaria()
+dealership.addCar(car1)
+dealership.addCar(car2)
+dealership.addCar(car3)
+dealership.addCar(car4)
+
+#Register users
+dealership.registerUser(user1) 
+dealership.registerUser(user2)
+
+#Show available cars
+dealership.showAvailableCars()
+
+#User rents a car
+user1.rentCar(car1)
+
+#User buys a car
+user2.buyCar(car2)
+
+#User tries to rent a car that is not available
+user1.rentCar(car1)
+
+#User tries to buy a car that is not available
+user2.buyCar(car2)
+
+#User returns a car
+car1.reBuy()
+
+#User rents a car again
+user1.rentCar(car1)
+#User buys a car again
+user2.buyCar(car2) 
+
+
 
 
 
